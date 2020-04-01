0e33a6c71e1d851b175de8fc31fbd09792887926
George Vigelette <gvigelet@duvitech.com>
2020-04-01 16:21:35 -0400

    removed unecessary items and added start command

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const app = express();
 const http = require('http').createServer(app)
-const crypto = require('crypto');
 const path = require("path");
-const jwt = require("jsonwebtoken");
-const rp = require("request-promise");
-const cookieParser = require('cookie-parser');
-const io = require('socket.io')(http);
 
-// Initialize the web app instance,
-app.use(cookieParser());
 // Indicate which directory static resources
 // (e.g. stylesheets) should be served from.
 app.use(express.static(path.join(__dirname, "html")));
 
-const port = process.env.PORT || 80;
-app.listen(port, function() {
-    console.log("Express server listening on port " + port);
-});
+const port = process.env.PORT || 1337;
+app.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
